38c03b1edab6334dc00ab5a1f1939a1b2b025044
Muhammad Yousaf <muhammad.yousaf6239@gmail.com>
2025-09-05 00:39:25 +0500

    Initial commit

---
+++ src/Components/ChatBox/ChatBox.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Chatbox.css'
+const ChatBox = () => {
+  return (
+    <div>ChatBox</div>
+  )
+}
+
+export default ChatBox
--- src/Pages/Chat/Chat.jsx
+++ src/Pages/Chat/Chat.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import './Chat.css'
+import LeftSidebar from '../../Components/LeftSidebar/LeftSidebar'
+import RightSidebar from '../../Components/RightSidebar/RightSidebar'
+import ChatBox from '../../Components/ChatBox/ChatBox'
 const Chat = () => {
   return (
-    <div>
-      we are on the chat app
+    <div className='chat'>
+      <div className="chat-container">
+        <LeftSidebar/>
+        <RightSidebar/>
+        <ChatBox/>
+      </div>
     </div>
   )
 }
