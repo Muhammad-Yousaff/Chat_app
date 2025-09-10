9b4d856391d39837b715eab3ec84d7bb6b9c5245
Muhammad Yousaf <muhammad.yousaf6239@gmail.com>
2025-09-10 23:12:35 +0500

    Working on ChatBox component UI and layout

--- src/Components/ChatBox/ChatBox.jsx
+++ src/Components/ChatBox/ChatBox.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-import './Chatbox.css'
+import React from 'react';
+import './Chatbox.css';
+import assets from '../../assets/assets';
+
 const ChatBox = () => {
   return (
-    <div>ChatBox</div>
-  )
-}
+    <div className="chat-box">
+      <div className="chat-user">
+        <img src={assets.profile_img} alt="Profile" />
+        <p>
+          Richard Sanford <img src={assets.green_dot} alt="Online" />
+        </p>
+        <img src={assets.help_icon} className="help" alt="Help Icon" />
+      </div>
+    </div>
+  );
+};
 
-export default ChatBox
+export default ChatBox;
--- src/Components/LeftSidebar/LeftSidebar.jsx
+++ src/Components/LeftSidebar/LeftSidebar.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
-import './LeftSidebar.css'
+import React from 'react';
+import './LeftSidebar.css';
+import assets from '../../assets/assets';
+
 const LeftSidebar = () => {
   return (
-    <div>LeftSidebar</div>
-  )
-}
+    <div className="ls">
+      <div className="ls-top">
+        <div className="ls-nav">
+          <img src={assets.logo} className="logo" alt="Logo" />
+          <div className="menu">
+            <img src={assets.menu_icon} alt="Menu Icon" />
+          </div>
+        </div>
 
-export default LeftSidebar
+        <div className="ls-search">
+          <img src={assets.search_icon} alt="Search Icon" />
+          <input type="text" placeholder="Search here..." />
+        </div>
+      </div>
+
+      <div className="ls-list">
+        {Array(12).fill("").map((_, index) => (
+          <div className="friend" key={index}>
+            <img src={assets.profile_img} alt="Profile" />
+            <div>
+              <p>Richard Sanford</p>
+              <span>Hello, how are you?</span>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default LeftSidebar;
