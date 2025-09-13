a9bf32a23587f018cf5f61766ef54276296e5eb9
Muhammad Yousaf <muhammad.yousaf6239@gmail.com>
2025-09-13 10:51:31 +0500

    Added input box feature

--- src/Components/ChatBox/ChatBox.jsx
+++ src/Components/ChatBox/ChatBox.jsx
@@ -6,11 +6,25 @@ const ChatBox = () => {
   return (
     <div className="chat-box">
       <div className="chat-user">
-        <img src={assets.profile_img} alt="Profile" />
+        {/* Profile image */}
+        <img src={assets.profile_img} alt="Profile" className="profile" />
+
+        {/* Username with green dot */}
         <p>
-          Richard Sanford <img src={assets.green_dot} alt="Online" />
+          Richard Sanford 
+          <img  src={assets.green_dot} alt="Online" className="green-dot" />
         </p>
+
+        {/* Help icon */}
         <img src={assets.help_icon} className="help" alt="Help Icon" />
+      </div>
+      <div className='chat-input'>
+        <input type="text" placeholder='Send a message' />
+        <input type="file" id='image' accept='image/png , image/jpeg' hidden/>
+        <label htmlFor="image"> 
+          <img src={assets.gallery_icon} alt="" />
+        </label>
+        <img src={assets.send_button} alt="" />
       </div>
     </div>
   );
--- src/Components/LeftSidebar/LeftSidebar.jsx
+++ src/Components/LeftSidebar/LeftSidebar.jsx
@@ -20,15 +20,16 @@ const LeftSidebar = () => {
       </div>
 
       <div className="ls-list">
-        {Array(12).fill("").map((_, index) => (
-          <div className="friend" key={index}>
-            <img src={assets.profile_img} alt="Profile" />
-            <div>
-              <p>Richard Sanford</p>
-              <span>Hello, how are you?</span>
-            </div>
-          </div>
-        ))}
+        {Array.from({ length: 12 }, (_, index) => (
+  <div className="friend" key={index}>
+    <img src={assets.profile_img} alt="Profile" />
+    <div>
+      <p>Richard Sanford</p>
+      <span>Hello, how are you?</span>
+    </div>
+  </div>
+))}
+
       </div>
     </div>
   );
